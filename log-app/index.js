2bab58e32a040c19ff9388b30f152375db0d0ba0
eggmkimura-svg <eggm.kimura@gmail.com>
2026-05-09 11:15:41 +0900

    Update index.js

--- log-app/index.js
+++ log-app/index.js
@@ -1,7 +1,29 @@
 (function() {
     'use strict';
     kintone.events.on('app.record.index.show', function(event) {
-        alert('kintoneと接続成功！');
+        // 表を表示するための場所（箱）を作る
+        const header = kintone.app.getHeaderMenuSpaceElement();
+        const displayDiv = document.createElement('div');
+        displayDiv.id = 'tabulator-table';
+        header.appendChild(displayDiv);
+
+        // テスト用のデータ
+        const tableData = [
+            {id:1, name:"テストスタッフ", role:"エンジニア"},
+            {id:2, name:"あなたの名前", role:"管理者"}
+        ];
+
+        // Tabulatorを起動！
+        new Tabulator("#tabulator-table", {
+            data: tableData, // データをセット
+            layout: "fitColumns", // 横幅いっぱいに広げる
+            columns: [ // 列の設定
+                {title:"ID", field:"id"},
+                {title:"名前", field:"name"},
+                {title:"役職", field:"role"},
+            ],
+        });
+
         return event;
     });
 })();
