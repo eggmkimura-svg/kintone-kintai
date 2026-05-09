17cf024954fcfb7af6c55d7ebe04d042ec108454
eggmkimura-svg <eggm.kimura@gmail.com>
2026-05-09 11:13:43 +0900

    Update index.js

--- log-app/index.js
+++ log-app/index.js
@@ -1,12 +1,7 @@
 (function() {
     'use strict';
-    // 一覧画面が表示された時に動く
     kintone.events.on('app.record.index.show', function(event) {
-        // 画面の一番上にメッセージを出す
-        const header = kintone.app.getHeaderMenuSpaceElement();
-        header.innerHTML = '<h1 style="padding:10px; background:#f4f4f4;">Tabulator準備完了！</h1>';
-        
-        console.log("kintoneとGitHubが繋がりました！");
+        alert('kintoneと接続成功！');
         return event;
     });
 })();
